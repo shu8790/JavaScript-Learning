32844d6d289673e7587548b814897c57b29f274d
shubham <srkale856@gmail.com>
2025-08-01 08:58:55 +0530

    added expalanation of var and let keyword

--- A_JS_Basic_Study/LetVarScopeStudy.js
+++ A_JS_Basic_Study/LetVarScopeStudy.js
@@ -1,32 +1,56 @@
 // scope of var keyword
-//      1. at global and functional level
+/*1. at global and functional level 
+->  it mean that if variable is define at global level then it is accessible anywhere in
+    code
+    if variable is define at functional level then it is accessible only in the specific function
+-> Redeclaring variables is allowed.
+        Redeclaring ->  let a= 4
+			            let a= “shubham”
+-> Reassigning of the variable is allowed.
+        Reassigning ->  let a= 4
+			                a=5
 
+*/
 var continent = "Asia"
 
 // block working of var
-
 if (1==1)
 {
     var continent ="America"
 }
 
+// function working of var
 function nameOfContinent()
 {
     var continent = "Africa"
 }
 
 console.log(continent)
-console.log("-----------------------------------")
+console.log("------------------------------------------------------------------------------------")
+
+// scope of let keyword
+/*1. at global and block level 
+->  it mean that if variable is define at global level then it is accessible anywhere in
+    code
+    if variable is define at functional level then it is accessible only in the specific function
+-> Redeclaring of variable is not allowed
+        Redeclaring ->  let a= 4
+			            let a= “shubham”
+-> Reassigning variables is allowed.
+        Reassigning ->  let a= 4
+			                a=5
+
+*/
 
 let state = "MH"
 
-// block working of var
-
+// block working of let
 if (1==1)
 {
     let state ="MP"
 }
 
+// function working of let
 function nameOfContinent()
 {
     let state = "TN"
